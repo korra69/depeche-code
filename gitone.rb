cba0931bfb4022803be955bbbcb1cc9f771a31f0
Leslie Di Mitri <lesliednyc@gmail.com>
2015-12-20 02:20:48 -0500

    first new branch commit

---
+++ gitone.rb
@@ -0,0 +1,13 @@
+puts "do you like poetry? Y/N?
+answer = gets.chomp.downcase
+
+   if (answer == 'y')
+	puts "roses are red, violets are too,"
+		"you obviously like coding, so this one's for you!"
+   else
+	puts nil
+end
+
+break
+
+end
